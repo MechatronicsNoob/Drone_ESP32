045137696cd618915c70d9a18317dc8eea1e3f6f
William Parker <wlhh.2001@gmail.com>
2023-03-05 00:45:43 -0500

    Editando mando

--- ControlRemoto/Mando_code/Mando_code.ino
+++ ControlRemoto/Mando_code/Mando_code.ino
@@ -1,19 +1,34 @@
 //---------------Librerías a utilizar-----------------
 //---------------Designación de pines-----------------
 // Joystick Derecho
+
 // Joystick Izquierdo
+
 // Encoder Derecho
+const int CLK_PIN_1 = 18; // GPIO18
+const int DT_PIN_1 = 19;  // GPIO19
+
 // Encoder Izquierdo
+const int CLK_PIN_2 = 22; // GPIO22
+const int DT_PIN_2 = 23;  // GPIO23
+
 // Potenciometer
+const int pot = 39; // GPIO39
 // Pulsadores
+const int btns = 36; // GPIO36
 
+void setup()
+{
+  pinMode(CLK_PIN_1, INPUT_PULLUP);
+  pinMode(DT_PIN_1, INPUT_PULLUP);
+  pinMode(CLK_PIN_2, INPUT_PULLUP);
+  pinMode(DT_PIN_2, INPUT_PULLUP);
 
-void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
-
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-
 }
