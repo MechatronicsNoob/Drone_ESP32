2605fc20917bb1bd5a0a51ec0da5e708d803fc06
William Parker <wlhh.2001@gmail.com>
2023-03-25 21:08:36 -0500

    Primera version del código para el mando

--- ControlRemoto/Mando_code/Funciones.ino
+++ ControlRemoto/Mando_code/Funciones.ino
@@ -1,1 +1,64 @@
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0){
+    success = "Delivery Success :)";
+  }
+  else{
+    success = "Delivery Fail :(";
+  }
+  Serial.println(">>>>>");
+}
 
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&receive_Data, incomingData, sizeof(receive_Data));
+  Serial.println();
+  battery_drone = receive_Data.battery_drone;
+  velocity_drone = receive_Data.velocity_drone;
+  altitude_drone = receive_Data.altitude_drone;
+  Serial.println("Receive Data: ");
+  Serial.println(battery_drone);
+  Serial.println(velocity_drone);
+  Serial.println(altitude_drone);
+}
+
+void resetData(){
+  send_Data.throttle = 0;
+  send_Data.yaw = 2048;
+  send_Data.pitch = 2048;
+  send_Data.roll = 2048;
+  send_Data.pot=0; //revisar si se envía correctamente o que valor va
+  send_Data.left_encoder=0;
+  send_Data.right_encoder=0;
+  send_Data.btn=0;
+  }
+
+long readEncoder(int CLK_PIN, int DT_PIN, long &encoderValue, int &lastCLKState, int &lastDTState) {
+  // Leemos el estado actual de los pines del encoder
+  int CLKState = digitalRead(CLK_PIN);
+  int DTState = digitalRead(DT_PIN);
+  
+  // Verificamos si el encoder ha girado en sentido horario
+  if (CLKState != lastCLKState && CLKState == HIGH) {
+    if (DTState == LOW) {
+      encoderValue++;
+    } else {
+      encoderValue--;
+    }
+    return encoderValue;
+  }
+  
+  // Verificamos si el encoder ha girado en sentido antihorario
+  if (DTState != lastDTState && DTState == HIGH) {
+    if (CLKState == LOW) {
+      encoderValue--;
+    } else {
+      encoderValue++;
+    }
+    return encoderValue;
+  }
+  
+  // Actualizamos el estado anterior de los pines del encoder
+  lastCLKState = CLKState;
+  lastDTState = DTState;
+}
--- ControlRemoto/Mando_code/Mando_code.ino
+++ ControlRemoto/Mando_code/Mando_code.ino
@@ -1,34 +1,133 @@
 //---------------Librerías a utilizar-----------------
+#include <esp_now.h>
+#include <WiFi.h>
+
+//---------------Dirección MAC-----------------
+uint8_t broadcastAddress[] = {0x7C, 0x9F, 0xFF, 0xFF, 0xFF, 0xFF}; //--> REPLACE WITH THE MAC Address of your receiver.
+
 //---------------Designación de pines-----------------
-// Joystick Derecho
+#define throttle_pin 32
+#define yaw_pin 33
+#define roll_pin 34
+#define pitch_pin 35
+#define pot_pin 39
+#define left_clk_pin 18
+#define left_dt_pin 19
+#define right_clk_pin 22
+#define right_dt_pin 23
+#define btn_pin 36
 
-// Joystick Izquierdo
+//---------------Creación de Variables-----------------
+int lastCLKState_left = LOW;
+int lastDTState_left = LOW;
+int lastCLKState_right = LOW;
+int lastDTState_right = LOW;
 
-// Encoder Derecho
-const int CLK_PIN_1 = 18; // GPIO18
-const int DT_PIN_1 = 19;  // GPIO19
+int battery_drone=0;
+int velocity_drone=0;
+int altitude_drone=0;
 
-// Encoder Izquierdo
-const int CLK_PIN_2 = 22; // GPIO22
-const int DT_PIN_2 = 23;  // GPIO23
+String success; //--> Variable to store if sending data was successful
 
-// Potenciometer
-const int pot = 39; // GPIO39
-// Pulsadores
-const int btns = 36; // GPIO36
+// Ejemplo de structure para enviar data
+typedef struct struct_sent{
+  long throttle;
+  long yaw;
+  long roll;
+  long pitch;
+  long pot;
+  long left_encoder;
+  long right_encoder;
+  bool btn;
+  }struct_sent;
+
+struct_sent send_Data;
+
+// Ejemplo de structure para recibir data
+typedef struct struct_received{
+  long battery_drone;
+  long velocity_drone;
+  long altitude_drone;
+  }struct_received;
+
+struct_received receive_Data;
 
 void setup()
 {
-  pinMode(CLK_PIN_1, INPUT_PULLUP);
-  pinMode(DT_PIN_1, INPUT_PULLUP);
-  pinMode(CLK_PIN_2, INPUT_PULLUP);
-  pinMode(DT_PIN_2, INPUT_PULLUP);
+  pinMode(throttle_pin, INPUT); 
+  pinMode(yaw_pin, INPUT);
+  pinMode(roll_pin, INPUT);
+  pinMode(pitch_pin, INPUT);
+  pinMode(pot_pin, INPUT);
+  pinMode(left_clk_pin, INPUT); // revisar si alguno es INPUT_PULLDOWN
+  pinMode(left_dt_pin, INPUT);
+  pinMode(right_clk_pin, INPUT);
+  pinMode(right_dt_pin, INPUT);
 
-  Serial.begin(9600);
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA); //--> Set device as a Wi-Fi Station
+  
+  //----------------------------------------Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  //----------------------------------------
+//----------------------------------------Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  //----------------------------------------
+  
+  //----------------------------------------Register peer
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  //----------------------------------------
+  
+  //----------------------------------------Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  //----------------------------------------
+  
+  esp_now_register_recv_cb(OnDataRecv); //--> Register for a callback function that will be called when data is received
+
+  resetData(); // Se llama a la función de reset para inicializar
 }
 
-void loop()
-{
-  // put your main code here, to run repeatedly:
+void loop(){
+  // Preparando los valores a enviar
+  long throttle = analogRead(throttle_pin);
+  long yaw = analogRead(yaw_pin);
+  long roll = analogRead(roll_pin);
+  long pitch = analogRead(pitch_pin);
+  long pot = analogRead(pot_pin);
+  long left_encoder = readEncoder(left_clk_pin,left_dt_pin, left_encoder, lastCLKState_left, lastDTState_left);
+  long right_encoder = readEncoder(right_clk_pin,right_dt_pin, right_encoder, lastCLKState_right, lastDTState_right);
+  bool btn;  
+
+  send_Data.throttle = throttle;
+  send_Data.yaw = yaw;
+  send_Data.pitch = pitch;
+  send_Data.roll = roll;
+  send_Data.pot=pot; 
+  send_Data.left_encoder=left_encoder;
+  send_Data.right_encoder=right_encoder;
+  send_Data.btn=btn;
+
+  Serial.println("Send data");
+
+  //----------------------------------------Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &send_Data, sizeof(send_Data));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  //----------------------------------------
+  
 }
